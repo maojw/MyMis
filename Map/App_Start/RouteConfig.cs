f3cdc6843910f2bbe0d0074a0cbdb9571d867973
maojw1124 <mjw1124@hotmail.com>
2015-02-08 21:58:57 +0800

    前端页面

--- Map/App_Start/RouteConfig.cs
+++ Map/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Map
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional },
                 namespaces: new string[] { "Map.Controllers" }
             );
         }

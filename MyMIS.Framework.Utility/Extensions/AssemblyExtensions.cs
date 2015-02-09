0d191d98d3a38b5ec2850f89d820ff421e8a18c9
maojw1124 <mjw1124@hotmail.com>
2015-02-09 22:42:42 +0800

    Utility项目完善

---
+++ MyMIS.Framework.Utility/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MyMIS.Framework.Utility.Extensions
+{
+    /// <summary>
+    /// 程序集扩展操作类
+    /// </summary>
+    public static class AssemblyExtensions
+    {
+        /// <summary>
+        /// 获取程序集的文件版本
+        /// </summary>
+        public static Version GetFileVersion(this Assembly assembly)
+        {
+            FileVersionInfo info = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return new Version(info.FileVersion);
+        }
+
+        /// <summary>
+        /// 获取程序集的产品版本
+        /// </summary>
+        public static Version GetProductVersion(this Assembly assembly)
+        {
+            FileVersionInfo info = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return new Version(info.ProductVersion);
+        }
+    }
+}
